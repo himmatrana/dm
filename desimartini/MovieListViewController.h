1a81e76e4da074f186cab870e315025be21381e1
Himmat Rana <rana.himmat@gmail.com>
2015-09-28 12:52:42 +0530

    movielist

---
+++ desimartini/MovieListViewController.h
@@ -0,0 +1,13 @@
+//
+//  MovieListViewController.h
+//  desimartini
+//
+//  Created by Himmat Rana on 28/09/15.
+//  Copyright © 2015 desimartini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MovieListViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *sidebarButton;
+@end
---
+++ desimartini/MovieListViewController.m
@@ -0,0 +1,46 @@
+//
+//  MovieListViewController.m
+//  desimartini
+//
+//  Created by Himmat Rana on 28/09/15.
+//  Copyright © 2015 desimartini. All rights reserved.
+//
+
+#import "MovieListViewController.h"
+#import "SWRevealViewController.h"
+
+@interface MovieListViewController ()
+
+@end
+
+@implementation MovieListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    SWRevealViewController *revealViewController = self.revealViewController;
+    if ( revealViewController )
+    {
+        [self.sidebarButton setTarget: self.revealViewController];
+        [self.sidebarButton setAction: @selector( revealToggle: )];
+        [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
+    }
+
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
