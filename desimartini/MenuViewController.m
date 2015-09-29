1981aad02f914170335f70634d9785996aa68ef7
Himmat Rana <rana.himmat@gmail.com>
2015-09-29 11:04:56 +0530

    menu items

--- desimartini/MenuViewController.m
+++ desimartini/MenuViewController.m
@@ -17,7 +17,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.menuItems = @[@"userinfo",@"home",@"movies",@"martini",@"news"]; //, @"movies", @"martini", @"news"];
+    self.menuItems = @[@"userinfo",@"home",@"movies",@"martini",@"news",@"blank",@"logout",@"feedback",@"rate_us",@"help"]; //, @"movies", @"martini", @"news"];
     
     
 }
